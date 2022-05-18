1f61683c18a18d1234f7390a642e38882910f8c8
kirktriplefive <kirkplat@gmail.com>
2022-05-18 21:50:00 +0300

    +init

--- develop/dev07/task.go
+++ develop/dev07/task.go
@@ -6,54 +6,42 @@ import (
 	"time"
 )
 
-func dev07(channels ...<-chan interface{}) <-chan interface{} {
-	merged := make(chan interface{})
-	var wg sync.WaitGroup
-	wg.Add(len(channels))
-	output := func(sc <-chan interface{}) {
-		for sqr := range sc {
-			merged <- sqr
-		}
-		wg.Done()
-	}
-	for _, optChan := range channels {
-		go output(optChan)
-	} 
-	wg.Add(1)
-	surveillance := func(sc <-chan interface{}) {
-		for sqr := range sc {
-			merged <- sqr
-		}
-		wg.Done()
-	}
-	for _, ch := range channels {
-		go surveillance(ch)
-	}
-	wg.Wait()
-	return merged
-}
-
 func main() {
-	var or func(channels ...<-chan interface{}) <-chan interface{}
-	or=dev07
-	sig := func(after time.Duration) <-chan interface{} {
-		c := make(chan interface{})
-		go func() {
-			defer close(c)
-			time.Sleep(after)
+	sig := func(after time.Duration) <-chan interface{} { // функция, создающая канал для чтения
+		c := make(chan interface{}) // канал для закрытия sig канала
+		go func() {                 // в горутине
+			defer close(c)             // закрываем канал с
+			time.Sleep(after)         
+			fmt.Println(after, "gone") 
 		}()
 		return c
 	}
-
-	start := time.Now()
-	<-or(
-		sig(2*time.Second),
-		sig(5*time.Second),
-		sig(1*time.Second),
-		sig(9*time.Second),
-		sig(8*time.Second),
+   
+	start := time.Now() 
+	<-or(               // вызываем функцию, которая будет принимать в себя каналы, созданные через sig
+		sig(1*time.Second), // создаем канал и закрываем его через секунду
+		sig(3*time.Second), // создаем канал и закрываем его через три секунды
+		sig(7*time.Second), 
+		sig(10*time.Second), 
 	)
+   
+	fmt.Printf("return after %v", time.Since(start))
+}
 
-	fmt.Printf("fone after %v", time.Since(start))
-
+func or(channels ...<-chan interface{}) <-chan interface{} {
+		var wg sync.WaitGroup         
+		output := make(chan interface{}) // создаем single-канал
+	
+		for i := range channels { 
+			wg.Add(1) // значения каждого канала будут вычитываться в своих горутинах
+			go func(input <-chan interface{}) {
+				for val := range input { // итерируемся по значениям канала
+					output <- val // пишем значение в единый канал
+				}
+				wg.Done() 
+			}(channels[i])
+		}
+		wg.Wait() 
+		close(output) //закрываем канал
+		return output // возвращаем канал
 }
---
+++ develop/dev08/task.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+	"syscall"
+	"github.com/mitchellh/go-ps"
+)
+
+func main() {
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		hat()
+		line, _ := reader.ReadString('\n')
+
+		if line == "exit\n" {
+			break
+		}
+		if line == "\n" {
+			continue
+		}
+
+		handle(line[:len(line)-1])
+	}
+}
+
+func handle(line string) {
+	subdata := strings.Split(line, fork)
+	for i := range subdata {
+		ret, _, err := syscall.Syscall(syscall.SYS_FORK, 0, 0, 0)
+		if err != 0 {
+			os.Exit(2)
+		}
+		if ret == 0 && len(subdata) == 1 {
+			// потомок, умирает если & не имеются
+			os.Exit(0)
+		}
+
+		if ret == 0 || len(subdata) == 1 {
+			// потомок, если & имеются
+			data := strings.Split(subdata[i], delim)
+			rez := " "
+			for j := range data {
+				var err error
+
+				rez, err = shell(data[j] + rez)
+				if err != nil {
+					println(err)
+				}
+			}
+			if len(rez) > 1 {
+				fmt.Println(rez[1:])
+			}
+			if ret == 0 {
+				os.Exit(0)
+			}
+		}
+	}
+}
+
+func shell(command string) (rez string, err error) {
+
+	args := strings.Fields(command)
+	if len(args) == 0 {
+		return "", nil
+	}
+	switch args[0] {
+	case "pwd":
+		rez, err = os.Getwd()
+	case "cd":
+		err = os.Chdir(args[1])
+	case "echo":
+		rez = args[len(args)-1]
+	case "ps":
+		var A, o bool
+		psargc := make([]string, 0)
+		for i := 0; i < len(args); i++ {
+			switch args[i] {
+			case "-A":
+				A = true
+			case "-o":
+				o = true
+				i++
+				buf := strings.Split(args[i], ",")
+				psargc = append(psargc, buf...)
+			}
+		}
+		var procs []ps.Process
+		if !o {
+			rez = "PID\tCMD\n"
+		}
+		procs, err = ps.Processes()
+		for _, p := range procs {
+			if os.Getppid() == p.Pid() || A {
+				if !o {
+					rez += fmt.Sprintf("%d\t%s\n", p.Pid(), p.Executable())
+				} else {
+					for j := range psargc {
+						switch psargc[j] {
+						case "pid":
+							rez += strconv.Itoa(p.Pid())
+						case "CMD":
+							rez += p.Executable()
+						default:
+						}
+					}
+				}
+			}
+		}
+	case "kill":
+		for i := range args {
+			if pid, _ := strconv.Atoi(args[i]); i > 0 {
+				syscall.Kill(pid, syscall.SIGINT)
+			} else {
+				println(err)
+			}
+		}
+	case "exec":
+		rez, err = exec(args[1])
+	}
+	return " " + rez, err
+}
+
+func exec(path string) (string, error) {
+	rez := ""
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		handle(path)
+	} else {
+		comm := strings.Split(string(data), "\n")
+
+		for i := 0; i < len(comm); i++ {
+			if comm[i] == "" {
+				continue
+			}
+			handle(comm[i])
+		}
+	}
+	return rez, nil
+}
+
+func hat() {
+	wd, _ := os.Getwd()
+	args := strings.Split(wd, "/")
+	fmt.Print(string(colorGreen), "shell /", string(colorBlue), args[len(args)-1], string(colorGreen), "$ ", string(colorWhite))
+}
+
+const (
+	delim      = "|"
+	fork       = "&"
+	colorGreen = "\033[32m"
+	colorBlue  = "\033[34m"
+	colorWhite = "\033[37m"
+)
